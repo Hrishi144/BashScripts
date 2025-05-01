07899513a56c72866a3e07c6502382bd1c8f3e84
hrishicote <166132248+Hrishi144@users.noreply.github.com>
2025-05-01 08:49:45 +0530

    Delete code/c/OPERATINGSYSTEM directory

--- code/c/OPERATINGSYSTEM/Semaphore.c
+++
@@ -1,3 +0,0 @@
-                                                                                                      #include<stdio.h>
-int mutex=1;full=0,empty=3;
-                                                                                                      
