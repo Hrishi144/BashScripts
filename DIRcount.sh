cb0b1a89ed7c6329b281067fef5eeb48ae6a9c0b
hrishi <hrishidileep@gmail.com>
2025-05-02 19:01:36 +0530

    gg

---
+++ DIRcount.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# --- Description Section ---
+echo
+green='\033[0;32m'
+NC='\033[0m'
+red='\033[1;91m'
+echo -e "${red}DESCRIPTION:${NC}"
+echo -e "${green}--------------------------------------${NC}"
+echo -e "${green}File Lister and Counter Script${NC}" ; 
+sleep 0.5
+echo -e "${green}This script lists and counts all files in a given directory.${NC}" ;
+sleep 0.5
+echo -e "${green}It accepts a directory path as a command-line argument.${NC}" ; 
+sleep 0.5
+echo -e "${green}It prints each file's full path and the total number of files.${NC}" ; 
+sleep 0.5
+echo -e "${green}Author: [Hrishikesh.D]${NC}" ;
+ sleep 0.5
+echo -e "${green}--------------------------------------${NC}"
+sleep 0.7
+
+ if [ -z "$1" ]; then
+    echo "This script uses command line arguments!!"
+    echo "Please enter a loaction"
+    echo  "How to use ->./file_name.sh  /DIR/location"
+    exit 1
+ fi
+
+path="$1"
+
+sleep 1
+
+if [ ! -d "$path" ]; then
+   echo " ERROR 404 Directory Not Found!! "
+   exit 1
+fi
+
+sleep 0.5
+echo -e "${red}!!!!!!**All Files in the directory [$path]**!!!!!!${NC}"
+for((i=0;i<50;i++)) do
+  echo -n "|"
+  sleep 0.1
+done
+echo
+
+
+find "$path" -type f
+
+NO_File=$(find "$path" -type f | wc -l)
+for((i=0;i<50;i++)) do
+  echo -n "|"
+   sleep 0.1
+done
+echo
+echo "NUMBER OF FILES IN DIRECTORY:-> $path:$NO_File"
+
+
+
+
+
+
